ff9ccf0bbc15b759d1d9f0e6e8d5f95ad02d64f3
exploitoverload <me@exploitoverload.com>
2023-07-05 09:23:18 +0200

    Updated zsh syntaxHighlighting and autosuggestions option definitions

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -17,8 +17,8 @@ in {
       dotDir = ".config/zsh";
 
       enableCompletion = true;
-      enableAutosuggestions = true;
-      enableSyntaxHighlighting = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
 
       shellAliases = {
         cat = "bat";
