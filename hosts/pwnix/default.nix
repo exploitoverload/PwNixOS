b9ca31cf20e6eab4d7049d506d90c96d8d9333dc
exploitoverload <me@exploitoverload.com>
2023-07-12 20:51:38 +0200

    Graphics: Default to intel only laptops

--- hosts/pwnix/default.nix
+++ hosts/pwnix/default.nix
@@ -2,8 +2,8 @@
 
 {
 	imports = [
-		../../modules/nvidia-optimus/default.nix # Choose nvidia-optimus/intel (AMD is WiP). Only laptops, i know.
-    # ../../modules/intel/default.nix
+    ../../modules/intel/default.nix # Default to intel graphics
+		# ../../modules/nvidia-optimus/default.nix # Enable this if you happen to have a nvidia optimus laptop
     ../../modules/podman/default.nix # Enable Podman
     ../../modules/neo4j/default.nix # Enable Neo4j
 	];
