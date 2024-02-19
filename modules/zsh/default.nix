6dba67092a12f4667555516858e54b6e0dbd0ac3
Asier Armenteros <me@exploitoverload.com>
2024-02-19 12:58:44 +0100

    Add zsh-nix-shell plugin to zsh.

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -8,7 +8,7 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       zsh
-        fzf
+      fzf
     ];
 
     programs.zsh = {
@@ -57,7 +57,16 @@ in {
         name = "sudo-zsh";
         src = ./.;
         file = "sudo.plugin.zsh";
-        }
+      }
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+      }
       ];
     };
   }; 
